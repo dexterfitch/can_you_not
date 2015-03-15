76150a3e03e6cfc6812710b92d2c22d831441a55
Dex Fitch <fitch.dexter@gmail.com>
2015-03-15 02:12:10 -0700

    Add controllers and views to handle editing, creating, and deleting lists.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,45 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      flash[:notice] = @list.name + " list successfully added!"
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      flash[:notice] = @list.name + " has been updated!"
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    destroyed_list_name = @list.name
+    @list.destroy
+    flash[:notice] = destroyed_list_name + " deleted!"
+    redirect_to lists_path
   end
 end
