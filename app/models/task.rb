6c146f9ac59e3de515fddc6c51333414f1ab3e30
Monty Rohde & Dex Fitch <student@epicodus.com>
2015-03-16 14:29:03 -0700

    Add task 'done' functionality.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,17 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
+
+
+  # before_create :set_done_to_false
+
+  # private
+  #
+  #   def set_done_to_false
+  #     self.done = false
+  #   end
+
+  # The above isn't working and we can't figure out why.
+  # Setting the default value of :done to false in the DB Migration seems to get it, though.
+
 end
